465a89c161a9c1301fecae8207d25a8759249771
Francis Awoh <realmabove02@gmail.com>
2023-05-23 09:58:52 +0000

    port configuration

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,12 +9,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.url_map.strict_slashes = False
 
-@app.route("/", strict_slashes=False)
+@app.route('/airbnb-onepage/')
+
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port='5000')
